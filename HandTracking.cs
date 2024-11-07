d8dd8c1c6f53566591b45ed9277248de95c94f23
Susanna Cifani <73530772+susannacifani@users.noreply.github.com>
2024-11-07 15:36:32 +0100

    Giroscopio funzionante

--- HandTracking.cs
+++ HandTracking.cs
@@ -6,42 +6,55 @@ using System;
 
 public class HandTracking : MonoBehaviour
 {
-    // Start is called before the first frame update
     public UDPReceiver udpReceiver;
     public GameObject cube;
 
-    // Fattore di smoothing per Lerp
-    float lerpFactor = 0.001f;
+    // Fattore di scala per il giroscopio (ipotizzando ±250°/s)
+    private float gyroscopeScaleFactor = 0.00763f;
 
-    void Start()
-    {
+    // Variabili di accumulo per la rotazione (in gradi)
+    private float accumulatedRotationX = 0f;
+    private float accumulatedRotationY = 0f;
+    private float accumulatedRotationZ = 0f;
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
         string data = udpReceiver.data;
 
+        // Verifica se i dati non sono nulli o vuoti
         if (string.IsNullOrEmpty(data))
             return;
 
-        // Rimuovere i caratteri iniziali e finali
+        // Rimuovere i caratteri iniziali e finali (assumendo che siano parentesi o altri caratteri)
         data = data.Remove(0, 1);
         data = data.Remove(data.Length - 1, 1);
 
         // Dividere i dati in base alla virgola
         string[] points = data.Split(',');
 
-        // Leggere i valori di rotazione dal giroscopio
-        float Gx = float.Parse(points[points.Length - 3]); // Gx
-        float Gy = float.Parse(points[points.Length - 2]); // Gy
-        float Gz = float.Parse(points[points.Length - 1]); // Gz
+        // Assicurarsi che ci siano abbastanza dati per leggere il giroscopio
+        if (points.Length < 3)
+            return;
 
-        // Creare la rotazione target
-        Quaternion targetRotation = Quaternion.Euler(Gx, Gy, Gz);
+        // Leggere i valori grezzi del giroscopio (Gx, Gy, Gz)
+        float Gx = float.Parse(points[points.Length - 3]);
+        float Gy = float.Parse(points[points.Length - 2]);
+        float Gz = float.Parse(points[points.Length - 1]);
 
-        // Interpolare tra la rotazione attuale e quella target
-        cube.transform.rotation = Quaternion.Lerp(cube.transform.rotation, targetRotation, lerpFactor);
+        // Convertire i valori del giroscopio in gradi per secondo
+        float gyroX = Gx * gyroscopeScaleFactor;
+        float gyroY = Gy * gyroscopeScaleFactor;
+        float gyroZ = Gz * gyroscopeScaleFactor;
+
+        // Integrare i valori del giroscopio per ottenere l'angolo di rotazione cumulativo (in gradi)
+        accumulatedRotationX += gyroX * Time.deltaTime;
+        accumulatedRotationY += gyroY * Time.deltaTime;
+        accumulatedRotationZ += gyroZ * Time.deltaTime;
+
+        // Creare la rotazione target usando i valori integrati
+        Quaternion targetRotation = Quaternion.Euler(accumulatedRotationX, accumulatedRotationY, accumulatedRotationZ);
+
+        // Applicare la rotazione al cubo
+        cube.transform.rotation = targetRotation;
     }
 }
